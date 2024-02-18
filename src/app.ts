bfd6626c39cd5d2d7ee8d305707d309a7d10fbf5
vmakar9 <zmerin212@gmail.com>
2024-02-18 15:06:25 +0200

    Init

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from "express"
-import cors from "cors"
 import {configs} from "./configs/config";
 import {weatherRouter} from "./router/weather.router";
 
@@ -9,12 +8,6 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
 
 app.use('/weather',weatherRouter)
